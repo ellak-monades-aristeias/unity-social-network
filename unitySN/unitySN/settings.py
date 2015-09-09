29bdeaf72e307d2e39d9fec7df6c36b0a522b963
mele25 <meletakis@gmail.com>
2015-09-09 12:06:18 +0300

    Added Grappeli and static files

--- unitySN/unitySN/settings.py
+++ unitySN/unitySN/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -101,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static/'
